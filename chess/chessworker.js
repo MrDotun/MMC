053a40a9e34e92132c2b6a362c6055ab6eed4231
MrDotun <dotunakinseye@gmail.com>
2026-05-09 23:07:48 +0100

    Create chessworker.js

---
+++ chess/chessworker.js
@@ -0,0 +1,19 @@
+// chessworker.js
+importScripts('engine/garbochess.js');
+
+self.onmessage = function(e) {
+    if (e.data.startsWith("position")) {
+        // substring(9) removes "position " to get the FEN
+        var fen = e.data.substring(9);
+        var result = InitializeFromFen(fen);
+        
+        if (result.length == 0) {
+            // Search at Depth 4
+            Search(function(bestMove, value, timeTaken, ply) {
+                if (bestMove != 0) {
+                    postMessage(FormatMove(bestMove)); 
+                }
+            }, 4, null);
+        }
+    }
+};
