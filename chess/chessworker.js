7ca0c3677fabfda3bdb653b5b49a26fcb42eecc3
MrDotun <dotunakinseye@gmail.com>
2026-05-09 23:18:27 +0100

    Update and rename chessworkers.js to chessworker.js

--- chess/chessworker.js
+++ chess/chessworker.js
@@ -5,10 +5,11 @@ self.onmessage = function(e) {
     if (e.data.startsWith("position")) {
         // substring(9) removes "position " to get the FEN
         var fen = e.data.substring(9);
+        
+        // Initialize the board with the puzzle FEN
         var result = InitializeFromFen(fen);
         
         if (result.length == 0) {
-            // Search at Depth 4
             Search(function(bestMove, value, timeTaken, ply) {
                 if (bestMove != 0) {
                     postMessage(FormatMove(bestMove)); 
